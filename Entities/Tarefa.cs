c3da3e053f974df873debcefa234413032ba20bc
Samuel Amom <samuel.amom01@gmail.com>
2024-03-13 23:47:34 -0300

    primeiro commit

---
+++ Entities/Tarefa.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciandoTarefas.Entities
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Data{get; set;}
+
+    }
+}
